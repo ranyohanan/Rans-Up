7efa1c1918ff037f77e3af30409be4c1d2269d3e
ranyohanan <yohananran3@gmail.com>
2023-08-08 10:00:59 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js";
+/* import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"; */
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
